579ba5609fcffc7aedfb8a4cc4eb49f0fe7b025d
ShakibRamez <shakibramiz1@icloud.com>
2025-06-16 03:37:44 +0430

    change style page worked and main.js file created

---
+++ dom_project/main.js
@@ -0,0 +1,9 @@
+ //change style page
+ function changeStyle() {
+      const el = document.getElementById("styleText");
+      el.classList.remove("text-gray-800");
+      el.classList.add("bg-black", "text-white", "font-bold");
+ }
+
+
+ 
